a66f825cf6f9d6084c0582705c603b85401508ca
Nikita <266900@via.dk>
2020-09-30 10:10:04 +0200

    plane detection, object spawn and finger movement

---
+++ Assets/Scripts/ARTapToPlaceObjectScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTapToPlaceObjectScript : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject gameObjectToInstantiate; //what we place in the space
+    private GameObject spawneObject;    
+    private ARRaycastManager _arRaycastManager;
+    private Vector2 touchPosition;  //where we touch with the finger
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    
+    
+
+     private void Awake() {
+         _arRaycastManager = GetComponent<ARRaycastManager>();
+
+     }
+
+     bool TryGetTouchPosition(out Vector2 touchPosition) {
+         if (Input.touchCount > 0 ) {
+               touchPosition = Input.GetTouch(0).position; 
+               return true;
+         }
+
+         touchPosition = default;
+         return false;
+     }
+    
+        
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition)) 
+        return;
+
+        if (_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon)) {
+            var hitPose = hits[0].pose;
+
+            if (spawneObject == null ) 
+            {
+                spawneObject = Instantiate(gameObjectToInstantiate, hitPose.position, hitPose.rotation) ;
+            }
+            else 
+            {
+                spawneObject.transform.position = hitPose.position;
+            }
+        }
+    }
+}
